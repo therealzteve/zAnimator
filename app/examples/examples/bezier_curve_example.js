ae15086d2f79116db5758615ea7efa91fb1c1cc4
Zteve <htczteve@gmail.com>
2016-11-01 10:36:16 +0100

    - worked on examples application

--- app/examples/examples/bezier_curve_example.js
+++ app/examples/examples/bezier_curve_example.js
@@ -4,11 +4,18 @@
 
 window.addEventListener('load', function(){
   var myAnimator = exampleRunner.getAnimator();
+  var startPoint = { 'x': 0, 'y': 0 };
   var bezierCurve = myAnimator.geometry.paths.bezierCurve({x: 0, y: 0}, {x: 100, y: 0}, {x: 0, y: -50}, {x: 100, y: -50});
   var customObject = myAnimator.factory.path(bezierCurve, "#F00");
 
   exampleRunner.addExample({
     path: ['shapes','paths'],
+    controls: [
+      {'name': 'Start point',
+       'type': 'coordinate',
+       'variable': startPoint
+     }
+    ],
     name: 'bezier curve example',
     start: function(){
       customObject.view.x = 300;
