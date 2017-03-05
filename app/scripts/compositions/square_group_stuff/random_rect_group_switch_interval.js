9b2324ba9f0ff1bce64adf91f4ce1d6d2931cf8c
Zteve <htczteve@gmail.com>
2017-03-05 23:50:58 +0100

    - Added random square group switch interval

---
+++ app/scripts/compositions/square_group_stuff/random_rect_group_switch_interval.js
@@ -0,0 +1,34 @@
+import randomRectGroupSwitch from './random_square_karo';
+import checkParameter from '~/internal/check_parameter';
+import timer from '~/timers/interval_timer';
+
+export default function(options){
+
+  checkParameter(options, 'interval', true);
+  checkParameter(options, 'columns', true);
+  checkParameter(options, 'visible', true);
+  checkParameter(options, 'spacing', false);
+  checkParameter(options, 'children', false, []);
+
+  var groupSwitch = randomRectGroupSwitch(options);
+  var groupSwitchTimer = timer(options.interval);
+  var switchInterval = {};
+
+  switchInterval.start = function(){
+    groupSwitchTimer.addListener(handle);
+    groupSwitchTimer.start();
+  };
+
+  switchInterval.stop = function(){
+    groupSwitchTimer.stop();
+    groupSwitchTimer.removeListener(handle);
+  };
+
+  switchInterval.view = groupSwitch.view;
+
+  function handle(){
+    groupSwitch.switch();
+  }
+
+  return switchInterval;
+}
--- app/scripts/compositions/square_group_stuff/square_group_stuff.js
+++ app/scripts/compositions/square_group_stuff/square_group_stuff.js
@@ -1,5 +1,7 @@
 import randomSquareSwitch from './random_square_karo';
+import randomSquareSwitchInterval from './random_rect_group_switch_interval';
 
 export default {
-  randomSquareSwitch: randomSquareSwitch
+  randomSquareSwitch: randomSquareSwitch,
+  randomSquareSwitchInterval: randomSquareSwitchInterval
 };
