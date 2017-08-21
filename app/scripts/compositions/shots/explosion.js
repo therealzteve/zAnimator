74e995044b7d60e71c75b9602386576ad4e86771
Zteve <htczteve@gmail.com>
2017-08-22 00:13:27 +0200

    added explosion shot

---
+++ app/scripts/compositions/shots/explosion.js
@@ -0,0 +1,35 @@
+import checkParameter from '~/internal/check_parameter';
+import component from '~/abstract_component';
+import container from '~/factories/createjs/components/container';
+import fadeShot from './fade_shot';
+import circle from '~/factories/createjs/components/circle';
+import circleShape from '~/geometry/shapes/circle';
+import linearMover from '~/filters/mover/point2point/linear';
+
+export default function(options){
+
+  checkParameter(options, 'amount', true);
+  checkParameter(options, 'speed', true);
+  checkParameter(options, 'fadeTime', true); // As an interval object
+
+  var explosion = component();
+  explosion.fadeTime = options.fadeTime;
+  explosion.amount = options.amount;
+  explosion.speed= options.speed;
+  explosion.view = container();
+
+
+  explosion.pulse = function(){
+    for(var i = 0; i < this.amount; i++){
+      var pointShot = fadeShot({'child': circle({'circleShape': circleShape({'radius': 1})}), 'interval': this.fadeTime});
+      var mover = linearMover({goalPoint: {x: ( this.fadeTime.getMs() / 1000 ) * this.speed, y: 0}, child: pointShot});
+      mover.start();
+      mover.view.rotation = Math.random() * 360;
+      pointShot.pulse(1);
+      this.view.addChild(mover.view);
+    }
+  };
+
+
+  return explosion;
+}
--- app/scripts/compositions/shots/shots.js
+++ app/scripts/compositions/shots/shots.js
@@ -1,7 +1,9 @@
 import echoCircle from './echo_circle';
 import fadeShot from './fade_shot';
+import explosion from './explosion';
 
 export default {
   echoCircle: echoCircle,
-  fadeShot: fadeShot
+  fadeShot: fadeShot,
+  explosion: explosion
 };
