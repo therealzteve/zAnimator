50735b44baaf6c6a10e7f0e5b9e779bfec0f5802
Steven Wruk <swruk@abasag.intra>
2017-12-04 13:09:54 +0100

    - Updated package.json, worked on rotation_shake shot

--- app/scripts/compositions/shots/rotate_shake.js
+++ app/scripts/compositions/shots/rotate_shake.js
@@ -1,23 +1,23 @@
 import checkParameter from '~/internal/check_parameter';
 import component from '~/abstract_component';
 import container from '~/factories/createjs/components/container';
-import rotationSwinger from '~/modificator/rotation/rotation_swinger';
+import rotationSwinger from '~/modificators/rotation/rotation_swinger';
+import intervalPulse from '~/modificators/pulse/interval_pulse';
 
 export default function(options){
 
-  checkParameter(options, 'length', true);
   checkParameter(options, 'range', true);
   checkParameter(options, 'shakeTime', true); // As an interval object
-  checkParameter(options, 'thickness', true); // As an interval object
+  checkParameter(options, 'child', true);
 
   var stickRotateShake = component();
   stickRotateShake.length = options.length;
   stickRotateShake.range = options.range;
-  stickRotateShake.shakeTime = options.shakeTime;
-  stickRotateShake.thickness = options.thickness;
+
   stickRotateShake.view = container();
   stickRotateShake.swinger = rotationSwinger({subject: stickRotateShake.view, range: 0});
   stickRotateShake.intervalPulse = intervalPulse({interval: stickRotateShake.shakeTime, subject: stickRotateShake.swinger, property: 'range', begin: 0, end: stickRotateShake.shake});
+  stickRotateShake.view.addChild(options.child);
 
   stickRotateShake.pulse = function(){
     this.intervalPulse.pulse(1);
